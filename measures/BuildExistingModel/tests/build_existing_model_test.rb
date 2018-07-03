a338b151321ce3ddb3162eb1fdb0a931860cc5cc
Joe Robertson <joseph.robertson@nrel.gov>
2018-07-03 14:39:58 -0600

    Trying to distribute across nodes.

--- measures/BuildExistingModel/tests/build_existing_model_test.rb
+++ measures/BuildExistingModel/tests/build_existing_model_test.rb
@@ -11,22 +11,41 @@ class BuildExistingModelTest < MiniTest::Test
     # this test applies all the residential measures using the build existing model measure
     # but exports the registered values to csv BEFORE running any simulations
     require 'parallel'
+
     runners = []
     characteristics_dir = File.absolute_path(File.join(File.dirname(__FILE__), "..", "..", "..", "lib", "housing_characteristics"))
     buildstock_csv = File.absolute_path(File.join(characteristics_dir, "buildstock.csv"))
-    num_rows = 0
-    CSV.foreach(buildstock_csv, headers:true) do |sample|
-      num_rows += 1
+    num_nodes, num_cores = nodes_and_cores_counts
+    
+    if num_nodes > 1
+      require 'mpi'
+      world = MPI::Comm::WORLD
+      node_rank = world.rank
+
+      split_csv(buildstock_csv, num_nodes)
+      new_buildstock_csv = File.join(File.dirname(buildstock_csv), "#{node_rank}_buildstock.csv")
+      
+    else
+      node_rank = 0
+      new_buildstock_csv = buildstock_csv
     end
-    Parallel.each([*1..num_rows], in_threads: processor_count) do |building_id|
-      puts "#{building_id} / #{num_rows}"
+    
+    building_ids = []
+    CSV.foreach(new_buildstock_csv, headers:true) do |sample|
+      building_ids << sample[0]
+    end
+
+    Parallel.each(building_ids, in_processors: num_cores) do |building_id|
+      puts "Building ID: #{building_id}"
       args_hash = {}
       args_hash["workflow_json"] = "measure-info.json"
       args_hash["number_of_buildings_represented"] = "80000000"
       args_hash["building_id"] = "#{building_id}"
+      args_hash["buildstock_csv"] = File.basename(new_buildstock_csv)
       runners << _test_measure(args_hash)
     end
-    CSV.open(File.join(File.dirname(__FILE__), "results.csv"), "w") do |csv|
+
+    CSV.open(File.join(File.dirname(__FILE__), "#{node_rank}_results.csv"), "w") do |csv|
       row = []
       result = runners[0].result
       result.stepValues.each do |arg|
@@ -46,22 +65,45 @@ class BuildExistingModelTest < MiniTest::Test
 
   private
   
-  def processor_count
+  def split_csv(filename, num_nodes, header_lines=1)
+
+    file = File.open(filename, "r")
+    size = (file.readlines.size / num_nodes).ceil
+
+    (1..num_nodes).each do |i|
+      new_filename = File.join(File.dirname(filename), "#{i-1}_#{File.basename(filename)}")
+      unless File.exist? new_filename
+        CSV.open(new_filename, "w") do |csv|
+          counter = 0
+          CSV.foreach(filename, headers:false) do |row|
+            counter += 1
+            if counter == 1
+              csv << row
+              next
+            end
+            next if (counter-1) <= (i-1) * size
+            csv << row
+            break if (counter-1) >= i * size
+          end
+        end
+      end
+    end
+  
+  end
+  
+  def nodes_and_cores_counts
     case RbConfig::CONFIG['host_os']
-    when /darwin9/
-      `hwprefs cpu_count`.to_i
-    when /darwin/
-      ((`which hwprefs` != '') ? `hwprefs thread_count` : `sysctl -n hw.ncpu`).to_i
     when /linux/
-      `cat /proc/cpuinfo | grep processor | wc -l`.to_i
-    when /freebsd/
-      `sysctl -n hw.ncpu`.to_i
+      num_nodes = ENV["PBS_NUM_NODES"].to_i
+      num_cores = `cat /proc/cpuinfo | grep processor | wc -l`.to_i
     when /mswin|mingw/
+      num_nodes = 1
       require 'win32ole'
       wmi = WIN32OLE.connect("winmgmts://")
       cpu = wmi.ExecQuery("select NumberOfCores from Win32_Processor")
-      cpu.to_enum.first.NumberOfCores
+      num_cores = cpu.to_enum.first.NumberOfCores
     end
+    return num_nodes, num_cores
   end
   
   def _test_measure(args_hash, num_infos=0, num_warnings=0)
